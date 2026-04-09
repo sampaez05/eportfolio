9f028a1b787e4caaa0eab09c5fa7152707dc2c5b
Samantha Paez <sampaez@udel.edu>
2026-04-09 12:54:17 -0400

    about me and home pages more fleshed out

---
+++ home/home.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {;
+    const viewProjectsButton = document.getElementById("viewProjectsButton");
+    //redirect to projects page if Click to See Projects Button is clicked
+    viewProjectsButton.addEventListener("click", function () {
+        //console.log("viewProjectsButton clicked")
+        window.location.href = "../projects/projects.html";
+    });
+});
+
