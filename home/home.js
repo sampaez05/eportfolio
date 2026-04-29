2caa4182b6d6070de91fe0a46a30e6cebde5c128
Samantha Paez <sampaez@udel.edu>
2026-04-29 14:23:45 -0400

    fix viewProjectsButton on home page

--- home/home.js
+++ home/home.js
@@ -1,9 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {;
-    const viewProjectsButton = document.getElementById("viewProjectsButton");
+/*document.addEventListener("DOMContentLoaded", function () {;
+    const viewProjectsButton1 = document.getElementById("viewProjectsButton1");
     //redirect to projects page if Click to See Projects Button is clicked
-    viewProjectsButton.addEventListener("click", function () {
-        //console.log("viewProjectsButton clicked")
+    viewProjectsButton1.addEventListener("click", function () {
+        console.log("viewProjectsButton1 clicked")
         window.location.href = "projects/projects.html";
     });
+});*/
+
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".viewProjectsButton");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", function () {
+            console.log("viewProjectsButton clicked");
+            window.location.href = "projects/projects.html";
+        });
+    });
 });
+
 
